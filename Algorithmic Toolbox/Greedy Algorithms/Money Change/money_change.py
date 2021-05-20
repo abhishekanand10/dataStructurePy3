6251d9a99c3435c92825b2bf520b78fd5e8f22c5
Abhishek Anand <abhishekanand@MacBook-Air.local>
2021-05-21 00:07:04 +0530

    change calculator problem

--- Algorithmic Toolbox/Greedy Algorithms/Money Change/money_change.py
+++ Algorithmic Toolbox/Greedy Algorithms/Money Change/money_change.py
@@ -3,12 +3,11 @@
 
 def money_change(money):
     assert 0 <= money <= 10 ** 3
-    t = 0 # total change count
-    den = [10,5,1]
-    i = 0 #counter for den
+    t = 0  # total change count
+    den = [10, 5, 1]
+    i = 0  # counter for den
 
-
-    while money >0:
+    while money > 0:
         t += int(money / den[i])
         money %= den[i]
         i += 1
